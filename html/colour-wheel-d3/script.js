410c8bb509fd661b3695bc73fd661dea98f50cd9
blackaeonium <83319369+blackaeonium@users.noreply.github.com>
2022-11-03 18:38:50 +1100

    added new colour wheel

---
+++ html/colour-wheel-d3/script.js
@@ -0,0 +1,6 @@
+// 2016-10-17: The source for this demo is now mentained as a Git repo.
+// See the full source code at:
+// https://github.com/benknight/kuler-d3
+
+var colorWheel = new ColorWheel();
+colorWheel.bindData(5);
